77a825925d106d10e3a8b2ac4d020a0519565bdb
Jonathan Jones <j.jones90@hotmail.co.uk>
2021-02-21 02:30:27 +0000

    cleaned up some code formatting

--- includes/Dbh.inc.php
+++ includes/Dbh.inc.php
@@ -4,18 +4,18 @@
    */
   class Dbh
   {
-  public $connection;
-  private $host = "localhost";
-  private $user = "root";
-  private $pass = "";
-  private $dbname = "complimentme";
+    public $connection;
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $dbname = "complimentme";
 
-  public function __construct() {
-    $this->connection = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
-  }
+    public function __construct() {
+      $this->connection = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+    }
 
-  public function query($sql) {
-    return $this->connection->query($sql);
-  }
+    public function query($sql) {
+      return $this->connection->query($sql);
+    }
   }
 ?>
